cda54f4c90faa8250417c7f4f49ec76147e58f11
Karungg <miftahfadilah71@gmail>
2023-07-21 02:29:42 +0000

    Membuat user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    public function login(): view
+    {
+    }
+
+    public function doLogin(): Response
+    {
+    }
+
+    public function doLogout(): Response
+    {
+    }
+}
